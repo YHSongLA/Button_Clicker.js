077bf38c7fcc46c1ee1b31d1fd0d6f2ff0e1c671
YHSongLA <jsong061@gmail.com>
2022-04-23 18:21:43 -0700

    Button Clicker

---
+++ Button_Clicker.js
@@ -0,0 +1,11 @@
+function logOut(element) {
+    element.innerText = "Logout";
+}
+
+function alertMe() {
+    alert('This page says Ninja was liked');
+}
+
+function removeButton(element) {
+    element.remove();
+}
